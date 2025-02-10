1c1692402f5db499cbc7b617dc1471b3b86ad9bf
Edgar Jiménez Bedolla <48222196+edgarjimenezb@users.noreply.github.com>
2025-02-10 16:10:58 -0600

    Fix smelly code

--- logger.py
+++ logger.py
@@ -3,20 +3,17 @@ from config import Config
 from os import getcwd
 import logging
 
-try:
-    #Se genera el Log con las configuraciones del INI de la aplicación
-    #Si no se pueden encontrar las configuraciones intenta crear un Log por default
-    #El Log se rola automáticamente al cambiar el día
-    configLog = Config.obtiene_config_log()
-    log = logging.getLogger(configLog.get('loggername', 'LogTipoCambio'))
-    loggerHandler = TimedRotatingFileHandler(configLog.get('path', getcwd() + "\\") + configLog.get('filename', 'tiposcambio2.log'), configLog.get('rotatetime', 'midnight'), 1, backupCount=5)
-    formatter = logging.Formatter('%(asctime)s|%(levelname)s|%(message)s')
-    loggerHandler.setFormatter(formatter)
-    loggerHandler.setLevel(configLog.get('loglevel', logging.DEBUG))
-    log.addHandler(loggerHandler)
-    log.setLevel(configLog.get('loglevel', logging.DEBUG))
-except Exception as ex:
-    raise
+#Se genera el Log con las configuraciones del INI de la aplicación
+#Si no se pueden encontrar las configuraciones intenta crear un Log por default
+#El Log se rola automáticamente al cambiar el día
+configLog = Config.obtiene_config_log()
+log = logging.getLogger(configLog.get('loggername', 'LogTipoCambio'))
+loggerHandler = TimedRotatingFileHandler(configLog.get('path', getcwd() + "\\") + configLog.get('filename', 'tiposcambio2.log'), configLog.get('rotatetime', 'midnight'), 1, backupCount=5)
+formatter = logging.Formatter('%(asctime)s|%(levelname)s|%(message)s')
+loggerHandler.setFormatter(formatter)
+loggerHandler.setLevel(configLog.get('loglevel', logging.DEBUG))
+log.addHandler(loggerHandler)
+log.setLevel(configLog.get('loglevel', logging.DEBUG))
 
-def getLogger():
+def get_logger():
     return log
